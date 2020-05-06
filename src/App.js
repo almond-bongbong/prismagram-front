b2f805e7748feb8628d7a489982100effdcbddd3
cmlee <bal.dongdongdong@gmail.com>
2020-05-07 00:28:23 +0900

    setting style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Theme from './styles/Theme';
+
+const Box = styled.div`
+  ${({ theme }) => theme.whiteBox};
+`;
 
 function App() {
-  return <div className="App">hello App</div>;
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      hello
+      <Box>hello box</Box>
+    </ThemeProvider>
+  );
 }
 
 export default App;
